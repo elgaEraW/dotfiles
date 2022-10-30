fb551436d24d5e908a36778201be85648a36cb52
Dev Joshi <devjoshi030@gmail.com>
2022-10-31 01:05:42 +0530

    Added packages needed with i3

--- i3-install.sh
+++ i3-install.sh
@@ -1,3 +1,12 @@
-sudo pacman -S xorg xorg-xinit lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings i3 terminator light
+#!/bin/bash
+
+sudo timedatectl set-ntp true
+sudo hwclock --systohc
+
+git clone https://aur.archlinux.org/yay.git
+cd yay/
+makepkg -si --noconfirm
+
+sudo pacman -S --noconfirm xorg xorg-xinit lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings i3 terminator light gpicview neovim ranger lxappearance playerctl zathura cmus smplayer scrot
 
 sudo systemctl enable lightdm
